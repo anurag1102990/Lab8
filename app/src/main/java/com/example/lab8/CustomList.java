f5fafb6a4073294c77b2427c1e6f0f73a6045d7a
anurag1102990 <anurag5@ualberta.ca>
2024-03-08 14:27:27 -0700

    delete_City test pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -70,5 +70,9 @@ public class CustomList extends ArrayAdapter<City> {
      * @throws IllegalArgumentException If the city is not in the list.
      */
     public void delete(City city) {
+        if (!cities.contains(city)) {
+            throw new IllegalArgumentException("City not found in the list.");
+        }
+        cities.remove(city);
     }
 }
